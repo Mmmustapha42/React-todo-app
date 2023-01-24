2bfc817b7a1fd7df48a395284123e410f74419e7
Mmmustapha42 <111079800+Mmmustapha42@users.noreply.github.com>
2023-01-24 23:53:02 +0100

    todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useEffect} from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  //const [id, setId] = useState()
+  const todoNameRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(){
+    const newTodo = [...todos]
+    const todo = newTodo.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodo)
+  }
+  
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if(name === '')return
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TodoList todos={todos}/>
+    <input ref={todoNameRef} type='text'></input>
+    <button onClick={handleAddTodo}> Add Todo</button>
+    <button> Clear Completed Todo</button>
+    <div> {todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
